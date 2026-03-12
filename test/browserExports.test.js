dead8f2bf0f4305a4987eb516182fcfbfe98d8eb
invakid404 <invakid404@riseup.net>
2026-03-12 07:51:41 -0600

    test: cover browser runtime export parity

--- test/browserExports.test.js
+++ test/browserExports.test.js
@@ -5,5 +5,11 @@ describe("browser entry exports", () => {
   test("re-exports optionalType from the browser entry", () => {
     expect(browser).toHaveProperty("optionalType");
     expect(browser.optionalType).toBe(main.optionalType);
+    expect(browser).toHaveProperty("CELFunction");
+    expect(browser.CELFunction).toBe(main.CELFunction);
+    expect(browser).toHaveProperty("listType");
+    expect(browser.listType).toBe(main.listType);
+    expect(browser).toHaveProperty("mapType");
+    expect(browser.mapType).toBe(main.mapType);
   });
 });
