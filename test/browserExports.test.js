4edf9478221ead13f0ba35c76c4dd8757463d37d
Tsvetomir Bonev <invakid404@riseup.net>
2026-03-12 08:03:47 -0600

    fix: align browser entry exports with main entry (#25)

---
+++ test/browserExports.test.js
@@ -0,0 +1,18 @@
+import * as browser from "../dist/browser.js";
+import * as main from "../dist/index.js";
+
+describe("browser entry exports", () => {
+  test(
+    "re-exports optionalType, CELFunction, listType, and mapType from the browser entry",
+    () => {
+    expect(browser).toHaveProperty("optionalType");
+    expect(browser.optionalType).toBe(main.optionalType);
+    expect(browser).toHaveProperty("CELFunction");
+    expect(browser.CELFunction).toBe(main.CELFunction);
+    expect(browser).toHaveProperty("listType");
+    expect(browser.listType).toBe(main.listType);
+    expect(browser).toHaveProperty("mapType");
+    expect(browser.mapType).toBe(main.mapType);
+    },
+  );
+});
